8b23ad2a29a5b8c2feb6f5f946a1a668b76a0699
Lance Dabre <lancedabre@icloud.com>
2026-03-21 17:22:48 +0530

    layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,14 +18,14 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Cinehoria | Free Open Source Web-Based Screenplay Editor ",
-  description: "A modern, free, open-source scriptwriting and screenplay software built for an ultrafocus enviornment. Developed by Lance Dabre. ",
+  title: "Cinehoria | Free Open Source Web-Based Screenplay Editor",
+  description: "A modern, free, open-source scriptwriting and screenplay software built for an ultrafocus environment. Developed by Lance Dabre.",
   keywords: ["cinehoria", "screenwriting software", "screenplay editor", "Lance Dabre", "Next.js Screenwriting software"],
-  authors: [{name: "Lance dabre", url: "https://github.com/lancedabre"}],
+  authors: [{name: "Lance Dabre", url: "https://github.com/lancedabre"}],
   openGraph: {
     title: 'Cinehoria - Screenwriting Software',
     description: 'A no BS screenwriting software to get you started.',
-    url: 'cinehoria.vercel.app',
+    url: 'https://cinehoria.vercel.app',
     siteName: 'Cinehoria',
     locale: 'en_US',
     type: 'website',
@@ -43,16 +43,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Flaticon Uicons - Bold Rounded (for the fi-br prefix) */}
         <link 
           rel='stylesheet' 
           href='https://cdn-uicons.flaticon.com/2.1.0/uicons-bold-rounded/css/uicons-bold-rounded.css' 
         />
       </head>
       <body
-      suppressHydrationWarning={true}  
+        suppressHydrationWarning={true}  
         className={`${geistSans.variable} ${geistMono.variable} ${courier.variable} antialiased`}
       >
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              "name": "Cinehoria",
+              "url": "https://cinehoria.vercel.app/"
+            })
+          }}
+        />
         {children}
       </body>
     </html>
